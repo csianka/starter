ccd812ea8738ac380b211f0c6b901f107784703f
Bianka Csitari <bia.csitari@gmail.com>
2024-08-15 14:28:44 +0200

    Functions

--- script_2.js
+++ script_2.js
@@ -12,7 +12,7 @@ if (hasDriversLicense) console.log("I can drive");
 
 //const if = 23; // same here
 */
-
+/*
 function logger() {
     console.log("My name is Bianka.");
 }
@@ -20,3 +20,42 @@ function logger() {
 logger();
 logger();
 logger();
+*/
+/*
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
+
+//function calAge1(birthYear) {
+//    const age = 2037 - birthYear;
+//    return age;
+//}
+/*
+// Function declaration
+function calAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calAge1(1991);
+console.log(age1);
+
+// Function expression
+const calAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calAge2(1991);
+
+console.log(age1, age2);
+*/
+
