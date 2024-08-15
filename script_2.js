9a51f0e0a9205242fe535afb116591ac827838a4
Bianka Csitari <bia.csitari@gmail.com>
2024-08-15 11:15:48 +0200

    first commit

---
+++ script_2.js
@@ -0,0 +1,22 @@
+"use strict" // it needs to be the very first code
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio"; // interface word is reserved by strict mode
+
+// const private = 543; // same here
+
+//const if = 23; // same here
+*/
+
+function logger() {
+    console.log("My name is Bianka.");
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
