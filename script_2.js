fefb274d5082076fee9470942fd609151fee929f
Bianka Csitari <bia.csitari@gmail.com>
2024-08-19 09:05:54 +0200

    Func tions summary

--- script_2.js
+++ script_2.js
@@ -59,3 +59,84 @@ const age2 = calAge2(1991);
 console.log(age1, age2);
 */
 
+/*
+// Arrow function
+const calAge3 = birthYear => 2037 - birthYear;
+const age3 = calAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+*/
+/*
+// Function calling another function
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+*/
+
+// Arrow function
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2024 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+console.log(yearsUntilRetirement(1971, "Erika"));
+
+// Function expression
+const yearsUntilRetirement2 = function (birthYear, firstName) {
+    const age = 2024 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
+}
+console.log(yearsUntilRetirement2(1992, "Bianka"));
+
+// Function declarination
+function yearsUntilRetirement3(birthYear, firstName) {
+    const age = 2024 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
+}
+console.log(yearsUntilRetirement3(1992, "Bianka"));
+
+// Function calling a function
+
+const calAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement4 = function (birthYear, firstName) {
+    const age = calAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years.`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired.`);
+        return -1;
+    }
+}
+console.log(yearsUntilRetirement4(1992, "Bianka"));
+console.log(yearsUntilRetirement4(1968, "Mike"));
+
+
+
